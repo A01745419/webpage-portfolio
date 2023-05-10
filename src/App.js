e0e67ca1cd0a3ef1a77b020240559b79b610bff3
José Luis Madrigal Sánchez <69483112+A01745419@users.noreply.github.com>
2023-05-10 14:37:57 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hola soy una App de React y quiero a Paulo
+    </div>
+  );
+}
+
+export default App;
